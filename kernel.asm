21356056d3a6fb8afd678055cbfebee5716ab964
kent-lopez9 <kent_lopez@dlsu.edu.ph>
2026-08-05 17:39:02 +0800

    Add files via upload

--- kernel.asm
+++ kernel.asm
@@ -19,35 +19,35 @@ global distance_asm
 
 distance_asm:
     push rbp
-    mov  rbp, rsp
-    add  rbp, 16        ; account for pushed rbp (+8) and return address (+8)
+    mov rbp, rsp
+    add rbp, 16        ; account for pushed rbp (+8) and return address (+8)
 
-    mov  r11, [rbp+32]  ; r11 = y2 pointer (5th parameter)
-    mov  rax, [rbp+40]  ; rax = z pointer (6th parameter)
-    xor  r10, r10       ; r10 = loop index i = 0
+    mov r11, [rbp+32]  ; r11 = y2 pointer (5th parameter)
+    mov rax, [rbp+40]  ; rax = z pointer (6th parameter)
+    xor r10, r10       ; r10 = loop index i = 0
 
 .loop:
-    cmp  r10, rcx       ; compare index i (r10) with n (rcx)
-    jge  .done
+    cmp r10, rcx       ; compare index i (r10) with n (rcx)
+    jge .done
 
-    ; --- dx = X2[i] - X1[i]; dx = dx*dx ---
-    movss xmm0, [r8 + r10*4]   ; xmm0 = X2[i]
-    subss xmm0, [rdx + r10*4]  ; xmm0 = X2[i] - X1[i]
-    mulss xmm0, xmm0           ; xmm0 = dx^2
+    ; dx = X2[i] - X1[i]; dx = dx*dx
+    movss xmm0, [r8 + r10*4] ; xmm0 = X2[i]
+    subss xmm0, [rdx + r10*4] ; xmm0 = X2[i] - X1[i]
+    mulss xmm0, xmm0 ; xmm0 = dx^2
 
-    ; --- dy = Y2[i] - Y1[i]; dy = dy*dy ---
-    movss xmm1, [r11 + r10*4]  ; xmm1 = Y2[i]
-    subss xmm1, [r9 + r10*4]   ; xmm1 = Y2[i] - Y1[i]
-    mulss xmm1, xmm1           ; xmm1 = dy^2
+    ; dy = Y2[i] - Y1[i]; dy = dy*dy
+    movss xmm1, [r11 + r10*4] ; xmm1 = Y2[i]
+    subss xmm1, [r9 + r10*4] ; xmm1 = Y2[i] - Y1[i]
+    mulss xmm1, xmm1 ; xmm1 = dy^2
 
-    ; --- Z[i] = sqrt(dx^2 + dy^2) ---
-    addss xmm0, xmm1           ; xmm0 = dx^2 + dy^2
-    sqrtss xmm0, xmm0          ; xmm0 = sqrt(dx^2 + dy^2)
-    movss [rax + r10*4], xmm0  ; Z[i] = xmm0
+    ; Z[i] = sqrt(dx^2 + dy^2)
+    addss xmm0, xmm1 ; xmm0 = dx^2 + dy^2
+    sqrtss xmm0, xmm0 ; xmm0 = sqrt(dx^2 + dy^2)
+    movss [rax + r10*4], xmm0 ; Z[i] = xmm0
 
-    inc  r10
-    jmp  .loop
+    inc r10
+    jmp .loop
 
 .done:
-    pop  rbp
+    pop rbp
     ret
